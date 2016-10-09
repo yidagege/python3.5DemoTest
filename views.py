f1b742e24578f3c7007d1192709cec4500f5af1c
zy <525153575@qq.com>
2016-10-09 17:47:27 +0800

    add

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request,"index.html")
+	block_infos = Block.objects.all().order_by("-id")
+	# block_infos = [{"name":"运维专区","desc":"运维学习讨论区","manager":"admin"},{"name":"Django专区","desc":"Django学习讨论区","manager":"admin"},{"name":"部落建设","desc":"有关部落建设的事宜","manager":"admin"}]
+	return render(request,"index.html",{"blocks":block_infos})
 
