86d30f82eedec49b6e10b77b2543ce9aaed84b44
zy <525153575@qq.com>
2016-10-17 00:50:20 +0800

    up

--- article/admin.py
+++ article/admin.py
@@ -3,7 +3,7 @@ from models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display = ("block","title","content","status")
+	list_display = ("block","title","content","create_timestamp","last_update_timestamp","status")
 
 
 admin.site.register(Article,ArticleAdmin)
--- article/views.py
+++ article/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from block.models import Block
+from models import Article
 
-# Create your views here.
+def article_list(request,block_id):
+	block_id = int(block_id)
+	block = Block.objects.get(id=block_id)
+	article_objs = Article.objects.filter(block=block,status=0).order_by("-id")
+	return render(request,"article_list.html",{"articles":article_objs,"b":block})
