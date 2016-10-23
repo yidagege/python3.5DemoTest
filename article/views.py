906bddb87df9ab5d6aafdf720ba07c0dd4035a95
zy <525153575@qq.com>
2016-10-23 22:17:31 +0800

    判断发表数据为空

--- article/views.py
+++ article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from block.models import Block
 from models import Article
+from django.db.models import Q
 
 def article_list(request,block_id):
 	block_id = int(block_id)
@@ -15,10 +16,13 @@ def article_publish(request,block_id):
 		article_objs = Article.objects.filter(block=block,status=0).order_by("-id")
 		return render(request,"article_publish.html",{"articles":article_objs,"b":block})
 	else:
-		title = request.POST["title"]
-		content = request.POST["content"]
-		block_id = int(block_id)
-		block = Block.objects.get(id=block_id)
-		article = Article(block=block,title=title,content=content,status=0)
-		article.save()
-		return redirect("/article/list/%s" % block_id)
+		if Q(request.POST["title"] == "")|Q(request.POST["content"] == ""):
+			return redirect("/article/publish/%s" % block_id)
+		else:
+			title = request.POST["title"]
+			content = request.POST["content"]
+			block_id = int(block_id)
+			block = Block.objects.get(id=block_id)
+			article = Article(block=block,title=title,content=content,status=0)
+			article.save()
+			return redirect("/article/list/%s" % block_id)
