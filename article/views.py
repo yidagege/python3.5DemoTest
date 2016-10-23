4a16401b8f64de53ff1377c72e50b8d77df7353a
zy <525153575@qq.com>
2016-10-23 23:07:40 +0800

    处理异常情况

--- article/views.py
+++ article/views.py
@@ -1,7 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render,redirect
 from block.models import Block
 from models import Article
-from django.db.models import Q
 
 def article_list(request,block_id):
 	block_id = int(block_id)
@@ -10,20 +10,18 @@ def article_list(request,block_id):
 	return render(request,"article_list.html",{"articles":article_objs,"b":block})
 
 def article_publish(request,block_id):
+	block_id = int(block_id)
+	block = Block.objects.get(id=block_id)
 	if request.method == "GET":
-		block_id = int(block_id)
-		block = Block.objects.get(id=block_id)
 		article_objs = Article.objects.filter(block=block,status=0).order_by("-id")
 		return render(request,"article_publish.html",{"articles":article_objs,"b":block})
 	else:
-		# Q(request.POST["title"] == "")|Q(request.POST["content"] == "")
-		if (request.POST["title"] == "") or (request.POST["content"] == ""):
-			return redirect("/article/publish/%s" % block_id)
-		else:
-			title = request.POST["title"]
-			content = request.POST["content"]
-			block_id = int(block_id)
-			block = Block.objects.get(id=block_id)
-			article = Article(block=block,title=title,content=content,status=0)
-			article.save()
-			return redirect("/article/list/%s" % block_id)
+		title = request.POST["title"]
+		content = request.POST["content"]
+		if not title or not content:
+			return render(request,"article_publish.html",{"b":block,"error":"标题和内容都不能为空！","title":title,"content":content})
+		if len(title)>100 or len(content)>10000:
+			return render(request,"article_publish.html",{"b":block,"error":"标题或内容太长了！","title":title,"content":content})
+		article = Article(block=block,title=title,content=content,status=0)
+		article.save()
+		return redirect("/article/list/%s" % block_id)
