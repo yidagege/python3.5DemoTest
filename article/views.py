c5698555f504ad498b55e2f8df9879be0a44bc43
zy <525153575@qq.com>
2016-10-25 00:21:23 +0800

    uo

--- article/views.py
+++ article/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render,redirect
 from block.models import Block
 from models import Article
+from forms import ArticleForm
 
 def article_list(request,block_id):
 	block_id = int(block_id)
@@ -16,12 +17,19 @@ def article_publish(request,block_id):
 		article_objs = Article.objects.filter(block=block,status=0).order_by("-id")
 		return render(request,"article_publish.html",{"articles":article_objs,"b":block})
 	else:
-		title = request.POST["title"]
-		content = request.POST["content"]
-		if not title or not content:
-			return render(request,"article_publish.html",{"b":block,"error":"标题和内容都不能为空！","title":title,"content":content})
-		if len(title)>100 or len(content)>10000:
-			return render(request,"article_publish.html",{"b":block,"error":"标题或内容太长了！","title":title,"content":content})
-		article = Article(block=block,title=title,content=content,status=0)
-		article.save()
-		return redirect("/article/list/%s" % block_id)
+		# title = request.POST["title"].strip()
+		# content = request.POST["content"].strip()
+		# if not title or not content:
+		# 	return render(request,"article_publish.html",{"b":block,"error":"标题和内容都不能为空！","title":title,"content":content})
+		# if len(title)>100 or len(content)>10000:
+		# 	return render(request,"article_publish.html",{"b":block,"error":"标题或内容太长了！","title":title,"content":content})
+		# article = Article(block=block,title=title,content=content,status=0)
+		# article.save()
+		# return redirect("/article/list/%s" % block_id)
+		form = ArticleForm(request.POST)
+		if form.is_valid():
+			article = Article(block=block,title=form.cleaned_data["title"],content=form.cleaned_data["content"],status=0)
+			article.save()
+			return redirect("/article/list/%s" % block_id)
+		else:
+			return render(request,"article_publish.html",{"b":block,"form":form})
